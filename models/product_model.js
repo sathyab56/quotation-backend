205a5b87e61fa34477293610b6e78781543089d3
sathyab56 <sathyasubramaniam80@gmail.com>
2025-05-15 14:23:34 +0530

    sivan1

--- models/product_model.js
+++ models/product_model.js
@@ -2,60 +2,87 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../configs/db.js";
 
 export const Product = sequelize.define("Product", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   company: {
     type: DataTypes.STRING,
     allowNull: true,
+    field: "company"
   },
   productName: {
     type: DataTypes.STRING,
     allowNull: false,
+    field: "product_name"
   },
   regularPrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "regular_price"
   },
   specialPrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "special_price"
   },
   transportIncluded: {
-    type: DataTypes.STRING, // Can be 'Yes' or 'No'
-    defaultValue: 'Yes',
+    type: DataTypes.STRING,
+    defaultValue: "Yes",
+    field: "transport"
   },
   purchaseGST: {
     type: DataTypes.STRING,
-    defaultValue: '5',
+    defaultValue: "5",
+    field: "purchase_gst"
   },
   transportPrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "transport_price"
   },
   distributorPrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "distributor_price"
   },
   specialSalePrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "special_sale"
   },
   institutionalPrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "institutional"
   },
   b2cPrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "b2c"
   },
   mrpPrice: {
     type: DataTypes.FLOAT,
     defaultValue: 0,
+    field: "mrp"
   },
   saleGST: {
     type: DataTypes.STRING,
-    defaultValue: '5',
+    defaultValue: "5",
+    field: "sale_gst"
   },
   priceType: {
     type: DataTypes.STRING,
-    defaultValue: 'regular',
+    defaultValue: "regular",
+    field: "price_type"
+  },
+  salePrice: {
+    type: DataTypes.FLOAT,
+    defaultValue: 0,
+    field: "sale_price"
   }
+}, {
+  tableName: "products",
+  timestamps: false
 });
